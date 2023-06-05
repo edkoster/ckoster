f73fc013ba9677a53de89f5a2b68eda17bb23bb7
Edwin Koster <e.koster@topgeschenken.nl>
2023-06-05 16:38:25 +0200

    +: Add randomize button

--- js/app.js
+++ js/app.js
@@ -1,12 +1,13 @@
 window.addEventListener("load", (event) => {
 
-    if (document.body.classList.contains('index')) {
-      initIndex();
-    }
+  if (document.body.classList.contains('index')) {
+    initIndex();
+  }
 
-    if (document.body.classList.contains('photos')) {
-      initGallery();
-    }
+  if (document.body.classList.contains('photos')) {
+    initGallery();
+    initRandomizer();
+  }
 });
 
 
@@ -20,11 +21,29 @@ function initIndex()
 
 function initGallery()
 {
-  alert('init!');
   lightGallery(document.getElementById('lightgallery'), {
     plugins: [lgAutoplay, lgFullscreen, lgThumbnail],
     licenseKey: 'your_license_key',
     speed: 500,
     slideShowInterval: 4000,
   });
+}
+function initRandomizer()
+{
+  let link = document.querySelector('#randomize');
+  link.addEventListener('click', function (event) {
+    event.preventDefault();
+    randomStartPoint();
+  });
+}
+
+function randomStartPoint()
+{
+  let gallery = document.querySelector('#lightgallery');
+  let photos = document.querySelectorAll('#lightgallery a');
+  let start = Math.floor(Math.random() * photos.length);
+
+  for (let i = 0; i< start; i++) {
+    gallery.appendChild(document.querySelector('#lightgallery a'));
+  }
 }
